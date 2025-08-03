4f18cb1b2ce11c8d9c24fa49e463544268e01d31
Sofía Alvarado <123610145+Sofia17266@users.noreply.github.com>
2025-08-03 14:32:33 -0600

    Commit Sofi

---
+++ FastParts/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace FastParts.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+
+        public ActionResult EditarPerfil()
+        {
+            return View();
+        }
+    }
+}
---
+++ FastParts/Controllers/MecanicoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace FastParts.Controllers
+{
+    public class MecanicoController : Controller
+    {
+        public ActionResult IngresoVehiculo()
+        {
+            return View();
+        }
+
+        public ActionResult DetallesServicio()
+        {
+            return View();
+        }
+    }
+}
