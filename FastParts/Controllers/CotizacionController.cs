42bea5d421346908c0c2633f2edb09222cb7be3c
karias2 <karias20999@ufide.ac.cr>
2025-08-20 21:04:29 -0600

    Impr: Mejoras Varias.

---
+++ FastParts/Controllers/CotizacionController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FastParts.Models;
+
+namespace FastParts.Controllers
+{
+    public class CotizacionController : Controller
+    {
+        //// GET: Cotizacion
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        private static List<Cotizacion> _cotizaciones = new List<Cotizacion>();
+        private static int _nextId = 1;
+
+        public ActionResult Lista()
+        {
+            return View(_cotizaciones);
+        }
+
+        public ActionResult Crear()
+        {
+            return View(new Cotizacion
+            {
+                Fecha = DateTime.Today
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Cotizacion model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = _nextId++;
+                _cotizaciones.Add(model);
+                return RedirectToAction("Lista");
+            }
+            return View(model);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            var cot = _cotizaciones.FirstOrDefault(c => c.Id == id);
+            if (cot == null) return HttpNotFound();
+            return View(cot);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Cotizacion model)
+        {
+            var cot = _cotizaciones.FirstOrDefault(c => c.Id == model.Id);
+            if (cot == null) return HttpNotFound();
+
+            cot.Cliente = model.Cliente;
+            cot.Fecha = model.Fecha;
+            cot.ServicioSolicitado = model.ServicioSolicitado;
+            cot.Observaciones = model.Observaciones;
+            cot.MontoEstimado = model.MontoEstimado;
+            cot.Aprobada = model.Aprobada;
+
+            return RedirectToAction("Lista");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            var cot = _cotizaciones.FirstOrDefault(c => c.Id == id);
+            if (cot == null) return HttpNotFound();
+            return View(cot);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        public ActionResult EliminarConfirmado(int id)
+        {
+            var cot = _cotizaciones.FirstOrDefault(c => c.Id == id);
+            if (cot != null) _cotizaciones.Remove(cot);
+            return RedirectToAction("Lista");
+        }
+
+
+    }
+}
---
+++ FastParts/Models/Cotizacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FastParts.Models
+{
+    public class Cotizacion
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public string ServicioSolicitado { get; set; }
+        public string Observaciones { get; set; }
+        public decimal MontoEstimado { get; set; }
+        public bool Aprobada { get; set; }
+    }
+}
