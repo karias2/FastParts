34d4fbd0c85c67aa6974a0bec5701e2042d97e57
Daniel Murillo <osacr6@gmail.com>
2025-09-30 17:16:56 -0600

    Encuesta de Satisfacción

--- FastParts/Migrations/Configuration.cs
+++ FastParts/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿namespace FastParts.Migrations
 {
     using FastParts.Models;
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -8,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(FastParts.Models.ApplicationDbContext context)
@@ -22,6 +23,56 @@
             {
                 context.Repuestos.Add(new RepuestoModel { Nombre = "Filtro de Aceite", Marca = "ACME", NumeroParte = "FA-100", Precio = 4500, Stock = 20, Proveedor = "Proveedor 1" });
                 context.Repuestos.Add(new RepuestoModel { Nombre = "Bujía", Marca = "SparkX", NumeroParte = "BX-200", Precio = 2500, Stock = 100, Proveedor = "Proveedor 2" });
+                context.SaveChanges();
+            }
+
+            if (!context.Encuestas.Any())
+            {
+                var EncuestaSatisfaccion = new EncuestaModel
+                {
+                    Nombre = "Encuesta de Satisfacción",
+                    Descripcion = "Encuesta para medir el nivel de satisfacción del cliente con el servicio de FastParts.",
+                    Preguntas = new List<PreguntaModel>()
+                };
+
+                context.Encuestas.Add(EncuestaSatisfaccion);
+                context.SaveChanges();
+
+
+                context.Preguntas.Add(new PreguntaModel
+                {
+                    ID_Encuesta = EncuestaSatisfaccion.ID_Encuesta,
+                    Descripcion = "¿Cómo califica la calidad del servicio?",
+                    Tipo = "Rango",
+                    Minimo = 1,
+                    Maximo = 5
+                });
+
+                context.Preguntas.Add(new PreguntaModel
+                {
+                    ID_Encuesta = EncuestaSatisfaccion.ID_Encuesta,
+                    Descripcion = "¿Cómo califica el tiempo de entrega?",
+                    Tipo = "Rango",
+                    Minimo = 1,
+                    Maximo = 5
+                });
+
+                context.Preguntas.Add(new PreguntaModel
+                {
+                    ID_Encuesta = EncuestaSatisfaccion.ID_Encuesta,
+                    Descripcion = "¿Cómo califica el trato recibido?",
+                    Tipo = "Rango",
+                    Minimo = 1,
+                    Maximo = 5
+                });
+
+                context.Preguntas.Add(new PreguntaModel
+                {
+                    ID_Encuesta = EncuestaSatisfaccion.ID_Encuesta,
+                    Descripcion = "Comentarios adicionales (opcional)",
+                    Tipo = "Texto"
+                });
+
                 context.SaveChanges();
             }
         }
