c4449e58f25055c4be7c47842081e86d698da824
karias2 <karias20999@ufide.ac.cr>
2025-11-10 23:02:27 -0600

    [Feat] : Creacion de nuevo Index con nuevo diseño y estetica. Rediseño de Layout.

--- FastParts/Controllers/HomeController.cs
+++ FastParts/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace FastParts.Controllers
             return View();
         }
 
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
