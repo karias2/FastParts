c074906617acf41d7f9f5e0d1337cae2f956f8db
Daniel Murillo <osacr6@gmail.com>
2025-11-11 18:15:12 -0600

    Update Startup.cs

--- FastParts/Startup.cs
+++ FastParts/Startup.cs
@@ -13,7 +13,6 @@ namespace FastParts
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            CrearRolesYUsuarios();
 
        
             app.CreatePerOwinContext(ApplicationDbContext.Create);
@@ -24,61 +23,6 @@ namespace FastParts
                 new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context.Get<ApplicationDbContext>())));
   
 
-        }
-
-        private void CrearRolesYUsuarios()
-        {
-            using (var context = ApplicationDbContext.Create())
-            {
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-
-                if (!roleManager.RoleExists("Admin"))
-                    roleManager.Create(new IdentityRole("Admin"));
-                if (!roleManager.RoleExists("Mecanico"))
-                    roleManager.Create(new IdentityRole("Mecanico"));
-                if (!roleManager.RoleExists("Cliente"))
-                    roleManager.Create(new IdentityRole("Cliente"));
-
-                if (userManager.FindByName("admin@fastparts.com") == null)
-                {
-                    var user = new ApplicationUser
-                    {
-                        UserName = "admin@fastparts.com",
-                        Email = "admin@fastparts.com",
-                        NombreCompleto = "Usuario Admin Por Defecto",
-                        Estado = true
-                    };
-                    var result = userManager.Create(user, "Admin123");
-                    if (result.Succeeded) userManager.AddToRole(user.Id, "Admin");
-                }
-
-                if (userManager.FindByName("mecanico@fastparts.com") == null)
-                {
-                    var user = new ApplicationUser
-                    {
-                        UserName = "mecanico@fastparts.com",
-                        Email = "mecanico@fastparts.com",
-                        NombreCompleto = "Usuario Mecanico Por Defecto",
-                        Estado = true
-                    };
-                    var result = userManager.Create(user, "Mecanico123");
-                    if (result.Succeeded) userManager.AddToRole(user.Id, "Mecanico");
-                }
-
-                if (userManager.FindByName("cliente@fastparts.com") == null)
-                {
-                    var user = new ApplicationUser
-                    {
-                        UserName = "cliente@fastparts.com",
-                        Email = "cliente@fastparts.com",
-                        NombreCompleto = "Usuario Cliente Por Defecto",
-                        Estado = true
-                    };
-                    var result = userManager.Create(user, "Cliente123");
-                    if (result.Succeeded) userManager.AddToRole(user.Id, "Cliente");
-                }
-            }
         }
     }
 }
